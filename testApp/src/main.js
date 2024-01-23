8ca548fe91750e90a6ee1a742904868888c87140
Lukáš Hejtmánek <hejtmy@gmail.com>
2024-01-23 13:09:59 +0100

    Adds testapp

---
+++ testApp/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:5000');
+const app = createApp(App);
+
+app.config.globalProperties.$socket = socket;
+
+app.mount('#app');
