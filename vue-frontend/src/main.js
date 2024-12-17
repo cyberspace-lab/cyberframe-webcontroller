08ee5d20eb780bf3dfa34c74e7b156a0639e44c0
Rikib1999 <74504682+Rikib1999@users.noreply.github.com>
2024-12-17 20:59:50 +0100

    switch to localhost

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
 
 import './assets/main.css';
 
-const socket = io('http://flask_app:4000');
+const socket = io('http://localhost:4000');
 
 socket.on('connect', () => {
     console.log('Vue connected to server');
